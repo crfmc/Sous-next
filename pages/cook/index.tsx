636a242d099d19ad70fe849d5f2b64b162c9bfe6
crfmc <crf85@cornell.edu>
2022-11-14 20:42:49 -0500

    create page for main cooking

---
+++ pages/cook/index.tsx
@@ -0,0 +1,15 @@
+import { Head } from "next/document";
+
+export default function Cook() {
+  return (
+    <div>
+      <h1>
+        Welcome to the best cooking app in the world.
+      </h1>
+      <h2>What would you like to cook today?</h2>
+      <a>Tutorial</a>
+      <a>Inventory</a>
+      <a>Get cooking!</a>
+    </div>
+  )
+}
