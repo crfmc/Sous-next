29a81493b3e04859e152bc0b17bb8eee95baf45a
crfmc <crf85@cornell.edu>
2022-11-15 22:45:26 -0500

    Create interface for NavigationLink

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
 import Head from "next/head";
+import Navigation from "../components/Navigation";
+
+export interface NavigationLink {
+  title?: string;
+  href: string;
+}
+
+const NavigationLinks: NavigationLink[] = [
+  { title: "Link1", href: "/" }
+]
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="">
       <Head>
         <title>Sous</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="main">
+      <main className="">
+        <Navigation
+          sticky={true}
+          links={ NavigationLinks }
+        />
         <h1>Hello! I&apos;m Sous.</h1>
         <a>Tutorial</a>
         <a>Inventory</a>
         <a>Get cooking!</a>
       </main>
 
-      <footer className="footer"></footer>
+      <footer className=""></footer>
     </div>
   )
 }
