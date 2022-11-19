97945c4433bbc0707039b347ddae013a4f296d83
crfmc <crf85@cornell.edu>
2022-11-19 16:46:45 -0500

    add text to pantry page

--- pages/pantry/index.tsx
+++ pages/pantry/index.tsx
@@ -5,6 +5,10 @@ export default function Inventory() {
     <>
       <h1>Welcome to your inventory</h1>
       <p>This is a place where you can update your kitchen inventory.</p>
+      <p>Select an ingredient to edit it's quantity.</p>
+
+
+      <button>Here's a short tutorial!</button>
     </>
   );
 }
