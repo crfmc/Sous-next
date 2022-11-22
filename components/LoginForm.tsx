fe10df97f3c2f0072bb7dd29f58437e658ca4c81
crfmc <crf85@cornell.edu>
2022-11-22 14:59:17 -0500

    create login form component

---
+++ components/LoginForm.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Login() {
+  return (
+    <div className="container">
+      <h1>
+        Log in
+      </h1>
+      <p>
+        By continuing, you agree to our <a>User Agreement</a> and <a>Privacy Policy</a>.
+      </p>
+      <form>
+      </form>
+    </div>
+  )
+}
