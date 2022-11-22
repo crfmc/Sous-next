7db871866168eb14742fede1bba3788dfd9adcd2
crfmc <crf85@cornell.edu>
2022-11-22 14:59:44 -0500

    create spae for alternative logins

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -9,6 +9,9 @@ export default function Login() {
       <p>
         By continuing, you agree to our <a>User Agreement</a> and <a>Privacy Policy</a>.
       </p>
+      <div className="alternative_logins_container">
+        
+      </div>
       <form>
       </form>
     </div>
