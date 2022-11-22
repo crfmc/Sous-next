20bfec87764303da4295fc2b8afb945c2f150fd1
crfmc <crf85@cornell.edu>
2022-11-22 15:00:12 -0500

    add login component to home page

---
+++ pages/login/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Navigation from "../../components/Navigation";
+
+
+export default function Login() {
+  return (
+    <div className="container">
+      <Head>
+        
+      </Head>
+      <Navigation sticky={false} links={[]}></Navigation>
+      <div className="background"></div>
+      
+    </div>
+  )
+}
