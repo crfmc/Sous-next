c18a9727646070c1bbe57cc287b690516fcf3d31
crfmc <crf85@cornell.edu>
2022-11-29 00:15:35 -0500

    Add navigation to cook page

--- pages/cook/index.tsx
+++ pages/cook/index.tsx
@@ -1,9 +1,11 @@
 import { Head } from "next/document";
+import Navigation from "../../components/Navigation";
 import Stepper from "../../components/Stepper";
 
 export default function Cook() {
   return (
     <div>
+      <Navigation />
       <h1>
         Welcome to the best cooking app in the world.
       </h1>
