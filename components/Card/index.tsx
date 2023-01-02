466508580e5ea8e8360bdb1932f45edde2972b61
crfmc <crf85@cornell.edu>
2023-01-01 23:49:20 -0500

    chore: rename card name to title

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,19 +1,19 @@
 import { z } from 'zod';
 
 const CardProps = z.object({
-    name: z.string(),
+    title: z.string(),
     mut: z.string().optional(),
 });
 
 type CardProps = z.infer<typeof CardProps>;
 
 
-export default function Card({ name, mut } : CardProps) {
+export default function Card({ title, mut } : CardProps) {
     return (
         <div className={"card" + " " + mut}>
             <div className={"card-container"}>
                 <div className={"card-container-content"}>
-                    <h1>{ name }</h1>
+                    <h1 className={"card-container-content-title"}>{ title }</h1>
                 </div>
             </div>
         </div>
