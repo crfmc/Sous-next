3bea2516ce66fe4d9745e9c07e9854825933ba14
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2023-02-02 12:53:04 -0500

    chore: add head element to stepper

--- pages/cook/index.tsx
+++ pages/cook/index.tsx
@@ -1,10 +1,13 @@
-import { Head } from "next/document";
+import Head from "next/head";
 import Navigation from "../../components/Navigation";
 import Stepper from "../../components/Stepper";
 
 export default function Cook() {
   return (
     <div>
+      <Head>
+        
+      </Head>
       <Navigation />
       <h1>
         Welcome to the best cooking app in the world.
