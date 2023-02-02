4e5f19cb6881e075942e445d8447f214c424e249
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2023-02-02 12:52:48 -0500

    feat: add event handler for step buttons

--- components/Stepper/index.tsx
+++ components/Stepper/index.tsx
@@ -1,28 +1,47 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 
 export default function Stepper() {
     const [step, setStep] = useState(0);
 
+    function handleClick(e : React.MouseEvent) {
+        e.preventDefault();
+        console.log('clicked', e);
+        // switch (e.target.)
+    }
+
     return (
         <div className="stepper-container">
-            {/* <div className="stepper-overlay top"></div>
-            <div className="stepper-overlay bottom"></div> */}
-            {/* MUST TURN THIS INTO UL FOR ACCESSIBILITY */}
+
             <ol className="stepper-text" start={0} dir="ltr">
-                <li className="stepper-text-block focused"><p>Prepare the materials.</p></li>
+                <div className="stepper-overlay top"></div>
+                <li data-step={0} className="stepper-text-block focused"><p>Prepare the materials.</p></li>
 
-                <li className="stepper-text-block"><p>Break the egg into a bowl</p></li>
+                <li data-step={1} className="stepper-text-block"><p>Break the egg into a bowl</p></li>
 
-                <li className="stepper-text-block"><p>Add Salt</p></li>
+                <li data-step={2} className="stepper-text-block"><p>Add Salt</p></li>
 
-                <li className="stepper-text-block"><p>Add Pepper</p></li>
+                <li data-step={3} className="stepper-text-block"><p>Add Pepper</p></li>
 
-                <li className="stepper-text-block"><p>Whisk ingredients in bowl</p></li>
+                <li data-step={4} className="stepper-text-block"><p>Whisk ingredients in bowl</p></li>
+                <div className="stepper-overlay bottom"></div>
             </ol>
 
+            
             <div className="stepper-controls">
-                <a href="#">Next</a>
+                {/* Make links accessible */}
+                <a 
+                    onClick={ (e) => handleClick(e) }
+                    className="stepper-controls-link prevStep"
+                    >
+                        <span className="stepper-controls-link-text prevStep-text">Previous</span>
+                </a>
+                <a 
+                    onClick={ (e) => handleClick(e) } 
+                    className="stepper-controls-link nextStep"
+                >
+                    <span className="stepper-controls-link-text prevStep-text">Next</span>
+                </a>
             </div>
         </div>
     )
